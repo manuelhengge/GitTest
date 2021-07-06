d02519692e91bd9307b55b9e7edfa69abd558f27
Manuel Hengge <manuel.hengge@jakob-brucker-gymnasium.de>
2021-07-06 14:40:44 +0200

    SPIELFELD wurde bearbeitet und SPIELSTEIN hinzugefügt

--- src/SPIELFELD.java
+++ src/SPIELFELD.java
@@ -3,5 +3,7 @@ public class SPIELFELD {
     public static void main(String[] args) {
         System.out.println("Hallo Welt");
         System.out.println("Wie geht es dir?");
+
+        //hier kommt eine Änderung
     }
 }
